5d1567bdb4e443fcec0c6b93ada2aa92b990e572
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2023-08-03 17:45:40 -0700

    Initial Code

---
+++ contracts/UnsafeReceiver.sol
@@ -0,0 +1,21 @@
+pragma solidity 0.8.19;
+
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+
+contract CCIPUNSAFE_RECEIVER is CCIPReceiver {
+
+    string public latestMessageText;
+    address public latestMessageSender;
+
+    constructor(address router) CCIP(router) {
+
+    }
+
+    function _ccipReceive(Client.Any2EVMMessage memory message) internal override {
+        latestMessageText = abi.decode(message.data, (string));
+        latestMessageSender = abi.decode(message.sender, (address));
+    }
+}
---
+++ contracts/UnsafeSender.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+
+contract CCIPUNSAFE_SENDER {
+
+    address link;
+    address router;
+
+    constructor(address _link) {
+        link = _link;
+        LinkTokenInterface(link).approve(router, type(uint256).max);
+    }
+
+    function send(address receriver,
+        uint64 destinationChainSelector,
+        string memory someText) external {
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage(
+            {
+                receiver: abi.encode(receiver),
+                data: abi.encode(someText),
+                tokenAmounts: new Client.EVMTokenAmount[](0),
+                extraArgs: "",
+                feeToken: ""
+            }
+        )
+
+        IRouterClient(router).ccipSend(destinationChainSelector, message);
+    }
+
+}
